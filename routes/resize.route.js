d860e9c873070f804d1100a1a84f68352e0a866b
David-Shibley <david.shibley@primetrust.com>
2023-09-29 12:49:38 -0600

    Adding sanitization

--- routes/resize.route.js
+++ routes/resize.route.js
@@ -1,9 +1,20 @@
 const ResizeController = require("../controllers/resize.controller");
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
-router.get("/resize/:path", async (req, res) => {
+const getResizeValidators = [
+  check("height").isNumeric().escape(),
+  check("width").isNumeric().escape(),
+  check("path").escape(),
+];
+
+router.get("/resize/:path", getResizeValidators, async (req, res) => {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
     const image = await ResizeController.resize(req);
     res.send(image);
   } catch (error) {
